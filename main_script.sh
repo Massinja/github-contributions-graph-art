8df6d43d3428514aae0064e6b25baebe9381bfaa
Elviie Abdulmennanova <el123abd@gmail.com>
2023-12-22 09:20:17 -0500

    describe the project, add starting point

---
+++ main_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Github contribution graph starts on Sundays, 7 squares high, ~50 squares wide;
+# 5 shades of green - the colour is individual to user's activity level 
+
+# Plan:
+# -get the Sunday date from the user to start from
+# -get the characters from the user from our predefined list
+# the list [A-Z 0-9 heart tree smile snowflake ! ? dash]
+# 
+
+# for example:
+# to display H:
+# commit for 7 days
+# skip 3 days, commit for 1 day, skip 3 days
+# skip 3 days, commit for 1 day, skip 3 days
+# commit for 7 days
+ 
+# to make a single space between characters:
+# skip 7 days
+
+
