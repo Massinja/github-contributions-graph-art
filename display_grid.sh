eee2b9929176a6ca5348e80ebd2e3c132a6a0489
Elviie Abdulmennanova <el123abd@gmail.com>
2023-12-27 14:10:43 -0500

    decide how to name variables

--- display_grid.sh
+++ display_grid.sh
@@ -5,9 +5,8 @@
  
 DAYS=5
 START_DATE="2020-01-01"
-STYLE=random
+STYLE=fixed
 NC=5
-UL=12
 
 WORK_DATE=$START_DATE
 for DAY in $(seq 1 $DAYS)
@@ -16,19 +15,13 @@ do
 	M=$(date -d $WORK_DATE +%m)
 	D=$(date -d $WORK_DATE +%d)
 
-	#number of commits(NC) on the same day is random or fixed
-	
-	if [ "$STYLE" == "random" ]; then
-		NC=$(( 1 + $RANDOM % $UL ))
-	fi
-
-	for i in $(seq 1 $NC)
+	for i in $(seq 1 $CMT)
 	do
-		echo "commit $DAY-$i for $PATRN grid" > $FILE
+		echo "commit $DAY-$i for full grid" > commits
 		export GIT_COMMITTER_DATE="$Y-$M-$D 12:0$i:00"
 		export GIT_AUTHOR_DATE="$Y-$M-$D 12:0$i:00"
-		git add $FILE
-		git commit --date="$Y-$M-$D 12:0$i:00" -m "add commit $DAY-$i to build $PATRN coverage github grid"
+		git add commits
+		git commit --date="$Y-$M-$D 12:0$i:00" -m "add commit $DAY-$i to build full coverage github grid"
 	done
    	WORK_DATE=$(date -d "$WORK_DATE +1 days" +%Y-%m-%d)
 
